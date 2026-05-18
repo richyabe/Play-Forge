130058d9009d574d780dda778525b77c490986c1
richyabe <richardabe857@gmail.com>
2026-05-18 12:23:23 +0100

    Add basic service worker for caching requests

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'playforge-v1';
+
+self.addEventListener('install', event => {
+  console.log('✅ Service Worker Installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
